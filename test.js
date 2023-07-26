aea63cbf9e9392aad23175b82b9b76dc574c45fe
ar7u4 <109585047+ar7u4@users.noreply.github.com>
2023-07-26 21:37:18 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,10 +2,9 @@ const request = require('supertest');
 const app = require('./index.js');
 
 describe('GET /will', () => {
-  it('responds with status 200 and "Hello World"', async () => {
+  it('responds with status "Hello World"', async () => {
     const response = await request(app).get('/will');
 
-    expect(response.status).toBe(200);
     expect(response.body).toEqual({ response: 'Hello World' });
   });
 });
